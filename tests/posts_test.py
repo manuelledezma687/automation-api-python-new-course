3c4236ffb0bb8a58398a818bb458bc9c8cad9b47
Manuel Ledezma <manuelledezma687@gmail.com>
2024-08-14 19:13:24 +0100

    tercera clase

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -1,19 +1,30 @@
 import requests
 import json
+import allure
+import pytest
 from config import Data
 from utils import read_file
 
+@allure.title("Obtener todos los Albumes")
+@allure.label("critical")
+@pytest.mark.smoke
 def test_get_all_albums() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
     print(response)
     assert response.status_code == 200
 
+@allure.title("Obtener un Album")
+@allure.label("critical")
+@pytest.mark.smoke
 def test_get_a_album() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
     albums = response.json() 
     title_content = [album['title'] for album in albums]
     assert 'quidem molestiae enim' in title_content 
 
+@allure.title("Crear un Post")
+@allure.label("critical")
+@pytest.mark.smoke
 def test_create_resource() -> None:
     payload = json.dumps(read_file.reader('post.json'))
     headers = {
