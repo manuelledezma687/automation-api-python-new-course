7fc0a9b80618c79ad3ad148687aa27dc3cf09589
Manuel Ledezma <92046228+manuelledezma687@users.noreply.github.com>
2024-08-15 13:41:29 +0100

    tercera clase

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -10,13 +10,12 @@ from utils import read_file
 @pytest.mark.smoke
 def test_get_all_albums() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
-    print(response)
     assert response.status_code == 200
 
 @allure.title("Obtener un Album")
 @allure.label("critical")
 @pytest.mark.smoke
-def test_get_a_album() -> None:
+def test_get_an_album() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
     albums = response.json() 
     title_content = [album['title'] for album in albums]
