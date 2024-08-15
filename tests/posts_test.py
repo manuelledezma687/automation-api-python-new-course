a83b406bcef44614ba4ff4b84353a503ff27f1a9
Manuel Ledezma <92046228+manuelledezma687@users.noreply.github.com>
2024-08-15 13:40:27 +0100

    segunda clase

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -5,10 +5,9 @@ from utils import read_file
 
 def test_get_all_albums() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
-    print(response)
     assert response.status_code == 200
 
-def test_get_a_album() -> None:
+def test_get_an_album() -> None:
     response = requests.get(Data.BASE_URL + "/albums", timeout=10)
     albums = response.json() 
     title_content = [album['title'] for album in albums]
