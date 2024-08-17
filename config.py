532d9414d5a2adbd7cb00658a6621fc9d6e94f7b
Manuel Ledezma <manuelledezma687@gmail.com>
2024-08-17 15:28:27 +0100

    quinta clase

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+env = os.getenv("ENV", "dev")
+dotenv_file = f".env.{env}"
+load_dotenv(dotenv_file)
 
 class Data:
-    BASE_URL = "https://jsonplaceholder.typicode.com"
+    BASE_URL = os.getenv("BASE_URL")
+    ENVIRONMENT = os.getenv("ENV")
